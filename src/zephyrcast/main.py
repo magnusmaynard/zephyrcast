dfc54a5f9b52b997bc713622b4ef96ec5db71af4
Magnus <contact@magnus.co.uk>
2025-03-11 13:01:06 +1300

    Allow command arg for changing model arch. Initial baseline model which doesnt work yet.

--- src/zephyrcast/main.py
+++ src/zephyrcast/main.py
@@ -6,7 +6,15 @@ from zephyrcast.models.model_harness import ModelHarness
 from zephyrcast.data.fetch import fetch_data
 from zephyrcast.data.prepare import prepare_data
 
-model = ModelHarness(arch="multivar", steps=6, target="0_wind_avg")
+DEFAULT_ARCH = "multivar"
+ARCH_OPTIONS = ["baseline", "multivar", "seq2seq"]
+TARGET = "0_wind_avg"
+STEPS = 6
+
+
+def get_model(arch=DEFAULT_ARCH):
+    return ModelHarness(arch=arch, steps=STEPS, target=TARGET)
+
 
 @click.group(
     help="Command line tool for using the zephyrcast model. This model is for local, short-term weather forecasting specifically for paragliding safety, using solely weather station data from Zephyr."
@@ -26,21 +34,48 @@ def prepare():
 
 
 @cli.command(help="Train zephyrcast model on prepared data.")
-def train():
+@click.option(
+    "--arch",
+    type=click.Choice(ARCH_OPTIONS),
+    default=DEFAULT_ARCH,
+    help="Model architecture to use for training.",
+)
+def train(arch):
+    model = get_model(arch)
     model.train()
 
+
 @cli.command(help="Evaluate zephyrcast model on prepared data.")
-def evaluate():
+@click.option(
+    "--arch",
+    type=click.Choice(ARCH_OPTIONS),
+    default=DEFAULT_ARCH,
+    help="Model architecture to use for evaluation.",
+)
+def evaluate(arch):
+    model = get_model(arch)
     model.evaluate()
 
+
 @cli.command(help="Run predictions using the trained zephyrcast model on new data.")
-@click.option("--live", is_flag=True, help="Predict on live data from the Zephyr API.")
+@click.option(
+    "--live", is_flag=True, help="Predict on live data from the Zephyr API."
+)
 @click.option(
     "--date",
-    type=click.DateTime(formats=["%Y-%m-%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%S"]),
+    type=click.DateTime(
+        formats=["%Y-%m-%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%S"]
+    ),
     help="Start datetime for prediction (format: YYYY-MM-DD, YYYY-MM-DDThh:mm:ss, or YYYY-MM-DD hh:mm:ss).",
 )
-def predict(live, date):
+@click.option(
+    "--arch",
+    type=click.Choice(ARCH_OPTIONS),
+    default=DEFAULT_ARCH,
+    help="Model architecture to use for prediction.",
+)
+def predict(live, date, arch):
+    model = get_model(arch)
     if date:
         start_datetime = (
             date if isinstance(date, datetime) else datetime.fromisoformat(date)
--- src/zephyrcast/models/baseline_model.py
+++ src/zephyrcast/models/baseline_model.py
@@ -1,9 +1,75 @@
-class BaselineModel():
-    def __init__():
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from skforecast.model_selection import backtesting_forecaster
+from skforecast.recursive import ForecasterRecursive
+
+
+class NaiveRegressor:
+    def fit(self, X, y):
+        pass  # No fitting needed for naive model
+
+    def predict(self, X):
+        import ipdb; ipdb.set_trace()
+        return X.iloc[-1]  # Predict the last known value
+
+
+class BaselineModel:
+    def __init__(self, steps: int, target: str):
+        self._steps = steps
+        self._target = target
+
+    def _get_model(self, data) -> ForecasterRecursive:
+        model = ForecasterRecursive(
+            regressor=NaiveRegressor(),
+            lags=1
+        )
+        model.fit(y=data[self._target])
+        return model
+
+
+    def train(self, data_train):
         pass
 
-    def train():
-        pass
+    def predict(self, data_test, start_date: datetime):
+        model =  self._get_model(data=data_test)
+        data= data_test
 
-    def predict():
-        pass
+        freq = data.index.freq
+
+        # TODO: assumes contiguous lags
+        last_window_start = start_date - ((len(model.lags) + 1) * freq)
+        last_window_end = start_date - freq
+        last_window_data = data.loc[last_window_start:last_window_end]
+
+        print(f"Last window: {last_window_start} to {last_window_end}")
+
+        predictions = model.predict(
+            last_window=last_window_data, steps=self._steps
+        )[self._target]
+        actuals = data.loc[predictions.index, self._target]
+
+        self._plot_predictions_vs_actuals(
+            predictions=predictions,
+            actuals=actuals,
+            historical_data=data.loc[
+                last_window_start:last_window_end, self._target
+            ],
+            start_date=start_date,
+            target_variable=self._target,
+            save_plot=True,
+        )
+
+    # def evaluate(self, data_test: pd.DataFrame):
+    #     metric, predictions = backtesting_forecaster(
+    #         forecaster=self._get_model(),
+    #         y=data[target_column],
+    #         initial_train_size=initial_train_size,
+    #         steps=steps,
+    #         metric='mean_squared_error',
+    #         refit=False,
+    #         verbose=True
+    #     )
+    #     print(f"Backtesting MSE: {metric}")
+    #     return metric, predictions
